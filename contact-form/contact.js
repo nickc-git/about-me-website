a5f2fef7141c18d8d1ce606f6a37fc3c8c3835ac
Nick C <73456922+nickc-git@users.noreply.github.com>
2022-08-08 15:37:34 -0400

    Updated Version 1.20

---
+++ contact-form/contact.js
@@ -0,0 +1,58 @@
+var sel = document.getElementById("program-list")
+function getSelectedOption() {
+  var opt;
+  for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+  opt = sel.options[i];
+  if ( opt.selected === true ) {
+      break;
+  }
+}
+return opt;
+}
+function sendMessage() {
+  var opt = getSelectedOption()
+
+  info = "None sent";
+  let theinfo = document.getElementById("email").value;
+  if (theinfo) {
+    info = theinfo;
+  } else {
+    info = "No email sent";
+  }
+
+  email = "None sent";
+  let theEmail = document.getElementById("email").value;
+  if (theEmail) {
+    email = theEmail;
+  } else {
+    email = "No email sent";
+  }
+
+  let themsg = document.getElementById("msg").value;
+  if (themsg) {
+    text = themsg;
+  } else {
+    text = "No message set";
+  }
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/1002531733859741767/jTmV3oWeLuOWZtlUQTgt1Cy5IpVRGKSRibRXBQ5F40HJAcj_FjRhFxdJq63k--_XNwyb");
+
+  request.setRequestHeader("Content-type", "application/json");
+
+  const params = {
+    username: "Client",
+    avatar_url: "",
+    content:
+      "<@285942521832407040> you have a client!, this is the msg: " +
+      text +
+      "  email is: " +
+      email +
+      " There contact info is: " +
+      info +
+      ". They want a: " +
+      opt.text,
+    discriminator: "7479",
+  };
+
+  request.send(JSON.stringify(params));
+}
